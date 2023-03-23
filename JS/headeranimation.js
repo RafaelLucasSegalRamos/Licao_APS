8a6ec79d2e60bbc805556345d95e3e047da7003f
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-03-23 11:37:16 -0300

    new header animation
    é uma nova animação que faz o header se encaixar quando usasse o scroll do mouse

---
+++ JS/headeranimation.js
@@ -0,0 +1,6 @@
+const cabecalho = document.querySelector('header')
+
+window.addEventListener("scroll",function(){
+    const cabecalho = document.querySelector('header');
+    cabecalho.classList.toggle('sticky', window.scrollY > 75);
+});
