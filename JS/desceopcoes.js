6a223b14e8427f935b9e80c86f8f58c5a16e7959
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-04-21 13:22:48 -0300

    no more error options

--- JS/desceopcoes.js
+++ JS/desceopcoes.js
@@ -2,12 +2,8 @@ const linkhome = document.getElementById('casa');
 const opcoes = document.querySelector('.options');
 
 linkhome.onmouseenter = function(){
-    const cabecalho = document.querySelector('header');
-
     opcoes.classList.toggle('active');
-    cabecalho.classList.toggle('opdown')
 }
 linkhome.onmouseleave = function(){
     opcoes.classList.toggle('active');
-    cabecalho.classList.toggle('opdown')
 }
