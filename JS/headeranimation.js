a69a9db8c965487a65a4bcf1a533275056bc3e3f
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-04-21 23:42:33 -0300

    Fixing sticky header

--- JS/headeranimation.js
+++ JS/headeranimation.js
@@ -2,5 +2,5 @@ const cabecalho = document.querySelector('header')
 
 window.addEventListener("scroll",function(){
     const cabecalho = document.querySelector('header');
-    cabecalho.classList.toggle('sticky', window.scrollY > 75);
+    cabecalho.classList.toggle('sticky', window.scrollY > 775);
 });
